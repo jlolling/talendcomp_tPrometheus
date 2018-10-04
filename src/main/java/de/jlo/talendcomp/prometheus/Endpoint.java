19446e9ed6965020db1cb8de03702db701bf88ea
Jan Lolling <jan.lolling@gmail.com>
2018-10-04 10:29:24 +0200

    First commit

---
+++ src/main/java/de/jlo/talendcomp/prometheus/Endpoint.java
@@ -0,0 +1,12 @@
+package de.jlo.talendcomp.prometheus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.prometheus.client.Gauge;
+
+public class Endpoint {
+	
+	private List<Gauge> listGauges = new ArrayList<Gauge>();
+
+}
